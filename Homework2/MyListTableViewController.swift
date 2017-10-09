64d88f4e0f3d0787bc24b74b9605b4659d5daae1
Akshay Ayyanchira <aayyanch@cci019gg82aws.dyn.uncc.edu>
2017-10-09 19:03:04 -0400

    Parsing with robust values done. Mapping to object is remaining

--- Homework2/MyListTableViewController.swift
+++ Homework2/MyListTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+
 class MyListTableViewController: UITableViewController {
 
     
@@ -34,12 +35,94 @@ class MyListTableViewController: UITableViewController {
     }
 
     func fetchData() {
-        var url = URL(string: "http://dev.theappsdr.com/apis/summer_2016_ios/data.json")
-        
+        let url = URL(string: "http://dev.theappsdr.com/apis/summer_2016_ios/data.json")
+        Alamofire.request(url!)
+            .responseJSON {response in
+                
+                //error checking
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling GET on /todos/1")
+                    print(response.result.error!)
+                    return
+                }
+                
+                //response check
+                guard let json = response.result.value as? [String: Any] else {
+                    print("didn't get todo object as JSON from API")
+                    print("Error: \(String(describing: response.result.error))")
+                    return
+                }
+                
+                //parse
+                guard let feedArray = json["feed"] as? [[String: Any]]  else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+//                print(feedArray)
+                
+                for feed in feedArray{
+                    
+                    //category parsing - Term
+                    if let category = feed["category"] as? [String:Any],
+                    let attributes = category["attributes"] as? [String:Any],
+                        let term = attributes["term"] as? String{
+                        print("category is \(term)")
+                    }
+                    
+                    //Author name
+                    if let artist = feed["artist"] as? [String:Any],
+                        let label = artist["label"] as? String{
+                            print("Author is \(label)")
+                    }
+                    
+                    //Title
+                    if let name = feed["name"] as? [String:Any],
+                        let label = name["label"] as? String{
+                        print("Title is \(label)")
+                    }
+                    
+                    //URL
+                    if let squareImage = feed["squareImage"] as? [Any],
+                    let firstObject = squareImage[0] as? [String:Any],
+                        let label = firstObject["label"] as? String{
+                            let thumbnailURL = URL(string: label)!
+                        print("Small image URL is \(String(describing: thumbnailURL))")
+                    }
+                    
+                    //Price
+                    if let price = feed["price"] as? [String:Any],
+                     let amount = price["amount"] as? String{
+                        print("Amount is \(amount)")
+                    }
+                   
+                    //Release date
+                    if let releaseDate = feed["releaseDate"] as? [String:Any],
+                        let label = releaseDate["label"] as? String{
+                        print("Release date is \(label)")
+                    }
+                    
+//                    if let category = feed["category"] as? String,
+//                        let price = feed["price"] as? String,
+//                        let releaseDate = feed["releaseDate"] as? String,
+//                        let squareImage = feed["squareImage"] as? URL,
+//                        let artist = feed["artist"] as? String,
+//                        let title = feed["name"] as? String{
+//
+//                        let feed:Product = Product(category: category , price: price as! Float, title: title , author: artist , releaseDate: releaseDate as! String, thumbnailURL: squareImage , otherImageURL: nil, productDescription: nil)
+//
+//                        print("object created for \(feed.title)")
+//                    }
+                }
+        }
     }
     
     // MARK: - Table view data source
 
+    @IBAction func reloadButtonPressed(_ sender: UIBarButtonItem) {
+        tableView.reloadData()
+        fetchData()
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
